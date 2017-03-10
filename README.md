c88fa9f64823f2a31fd5ef370d2dbcb77acee5de
Sergei Stepanov <sergei.stepanov@ysura.com>
2017-03-10 19:09:21 +0100

    Added Copyright, README, gitignore, authors

---
+++ README.md
@@ -0,0 +1,26 @@
+HotSwapAgent-IntelliJ-IDEA-plugin
+=================================
+
+### What's in the sources
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan. 
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna. 
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus. 
+ * Vestibulum commodo. Ut rhoncus gravida arcu. 
+
+### More details
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan. 
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna. 
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus. 
+ * Vestibulum commodo. Ut rhoncus gravida arcu. 
+
+### Extra steps:
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan. 
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna. 
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus. 
+ * Vestibulum commodo. Ut rhoncus gravida arcu. 
--- src/com/hotswap/agent/plugin/services/DownloadManager.kt
+++ src/com/hotswap/agent/plugin/services/DownloadManager.kt
@@ -1,3 +1,18 @@
+/*
+ *  Copyright (c) 2017 Dmitry Zhuravlev, Sergei Stepanov
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.hotswap.agent.plugin.services
 
 import com.google.gson.JsonArray
@@ -29,12 +44,11 @@ import java.util.concurrent.TimeUnit
  * @author Dmitry Zhuravlev
  *         Date:  10.03.2017
  */
-
 class DownloadManager {
     companion object {
         internal val log = Logger.getInstance(DownloadManager::class.java)
         fun getInstance(project: Project? = null) = when (project) {
-            null -> ServiceManager.getService<DownloadManager>(ProjectManager.getInstance().defaultProject,DownloadManager::class.java)!!
+            null -> ServiceManager.getService<DownloadManager>(ProjectManager.getInstance().defaultProject, DownloadManager::class.java)!!
             else -> ServiceManager.getService<DownloadManager>(project, DownloadManager::class.java)!!
         }
     }
